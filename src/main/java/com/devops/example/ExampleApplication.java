6408149ce18157234174022fdc09113c046a1703
Leandro Tombetta <leandro.tombetta87@gmail.com>
2024-01-15 18:07:40 -0300

    test processes about jenkins

--- src/main/java/com/devops/example/ExampleApplication.java
+++ src/main/java/com/devops/example/ExampleApplication.java
@@ -1,13 +1,25 @@
 package com.devops.example;
 
+import static org.slf4j.LoggerFactory.*;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+
 @SpringBootApplication
 @RestController
-public class ExampleApplication {
+@Slf4j
+public class ExampleApplication implements CommandLineRunner {
+
+
+
 
 	@GetMapping
 	public String message(){
@@ -15,7 +27,12 @@ public class ExampleApplication {
 	}
 
 	public static void main(String[] args) {
+		log.info("it's continues integration job");
 		SpringApplication.run(ExampleApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("it's my second logger");
+	}
 }
--- src/test/java/com/devops/example/ExampleApplicationTests.java
+++ src/test/java/com/devops/example/ExampleApplicationTests.java
@@ -1,13 +1,21 @@
 package com.devops.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
+@Slf4j
 class ExampleApplicationTests {
+
 
 	@Test
 	void contextLoads() {
+		log.info("it's test about continuous integration");
+        assertTrue(true);
 	}
 
 }
